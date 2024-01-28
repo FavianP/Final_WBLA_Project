8d9a46701dbe475ffac6e0e774cee77493d5422b
FavianP <fperez593@west-mec.org>
2024-01-28 14:23:32 -0700

    first commit

---
+++ public/WBLA_VSCODE.2/smoothScroll.js
@@ -0,0 +1,17 @@
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach(link=>{
+    link.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const id = e.target.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+    const vhInPixels = window.innerHeight * 0.20;
+    let Position = element.offsetTop - vhInPixels;
+
+    window.scrollTo({
+left:0,
+top:Position;
+
+    })
+
+    })
+})
